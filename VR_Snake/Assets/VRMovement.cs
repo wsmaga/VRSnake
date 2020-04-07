0efe3fbace7bac7f5b616f44dead773a3c51e425
JChrobak <jonatanchrobak@gmail.com>
2020-04-07 18:30:32 +0200

    Alpha Version 1.0 Added player movement Added sample map

---
+++ VR_Snake/Assets/VRMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class VRMovement : MonoBehaviour
+{
+    private const float vectorResize = 0.01f;
+    public float mSpeed = 3.0f;
+    public float gravity = 2.0f;
+    private bool isMoving = true;
+    private CharacterController characterController;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Transform snakeHead;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            //snakeHead.position = playerTransform.position;
+            snakeHead.rotation = Quaternion.Euler(0, cameraTransform.localEulerAngles.y, 0);
+            Vector3 vForward = snakeHead.transform.forward*vectorResize;
+            //snakeHead.position = snakeHead.position + vForward/vectorResize;
+            vForward.y = -gravity*vectorResize;
+            characterController.Move(vForward*mSpeed);
+
+        }
+
+    }
+}
