ac0987e0dcab7907015b33cb55c2e56c0b6a6f9c
JChrobak <jonatanchrobak@gmail.com>
2020-05-13 21:51:29 +0200

    Added snake trail generation script Minor movement script changes

--- VR_Snake/Assets/FrontCollisionDetection.cs
+++ VR_Snake/Assets/FrontCollisionDetection.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class FrontCollisionDetection : MonoBehaviour
 {
-    
-    private void OnTriggerEnter(Collider other)
+
+    private void OnCollisionEnter(Collision collision)
     {
         //sprawdź czy wywołuje to faktyczna głowa czy klon tworzony po śmierci (klon to sama kostka i jej obiekty dzieci więc jeżeli rodzic tego rodzica jest null to znaczy że jesteśmy w klonie)
         if (this.transform.parent.parent != null)
         {
             Debug.Log("Collision from front collider");
-            this.transform.parent.transform.parent.GetComponent<VRMovement>().CollisionHandler(other.gameObject);
+            this.transform.parent.transform.parent.GetComponent<VRMovement>().CollisionHandler(collision.gameObject);
         }
         //usuń skrypt z klona
         else
             Destroy(this.GetComponent<FrontCollisionDetection>());
     }
+    
+    
 }
