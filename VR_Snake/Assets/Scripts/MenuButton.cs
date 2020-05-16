5a1f2bbd25539b3583f82278bed00da42f0bd1d7
wsmaga <witosma861@student.polsl.pl>
2020-05-16 11:16:54 +0200

    Main menu Added scene with game's main menu along with: - working gaze UI: standard Google raycaster + custom reticle and circle indicator - user controls: map testing, map select and game exit - UI controller object with scripting(including loading different scenes at runtime) - some necessary custom textures

---
+++ VR_Snake/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Threading;
+using System;
+
+public class MenuButton : MonoBehaviour
+{
+    public Image circleIndicator;
+    public UnityEvent buttonClickEvent;
+    public float clickTime = 1;
+    bool status = false;
+    public float gazeTimer = 0;
+
+    private void Start()
+    {
+        Debug.Log("BUTTON STARTED");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (status)
+        {
+            gazeTimer += Time.deltaTime;
+            circleIndicator.fillAmount = gazeTimer / clickTime;
+        }
+
+        if(gazeTimer > clickTime)
+        {
+            status = false;
+            circleIndicator.fillAmount = 0;
+            gazeTimer = 0;
+            Debug.Log("BUTTON CLICKED");
+            buttonClickEvent.Invoke();
+        }
+    }
+
+    public void GazeOn()
+    {
+        Debug.Log("GAZE ON");
+        status = true;
+    }
+
+    public void GazeOff()
+    {
+        Debug.Log("GAZE OFF");
+        status = false;
+        gazeTimer = 0;
+        circleIndicator.fillAmount = 0;
+    }
+}
---
+++ VR_Snake/Assets/Scripts/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    Canvas mainMenu;
+
+    [SerializeField]
+    Canvas mapSelect;
+
+    public void Start()
+    {
+        ShowMainMenu();
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("EXITING APPLICATION");
+        Application.Quit(0);
+    }
+
+    public void ShowMapSelect()
+    {
+        mainMenu.gameObject.SetActive(false);
+
+        mapSelect.gameObject.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        mapSelect.gameObject.SetActive(false);
+
+        mainMenu.gameObject.SetActive(true);
+    }
+
+    public void StartMap(string map)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(map, UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+}
