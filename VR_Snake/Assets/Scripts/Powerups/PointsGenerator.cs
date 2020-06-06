05a9716220a346ec6f475d83e127fd098502f64c
Adam Puchała <adam.puchala@outlook.com>
2020-06-06 15:14:27 +0200

    Fixed inaccessible places point spawn and added points array to script

--- VR_Snake/Assets/Scripts/Powerups/PointsGenerator.cs
+++ VR_Snake/Assets/Scripts/Powerups/PointsGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class PointsGenerator : MonoBehaviour
@@ -10,6 +9,9 @@ public class PointsGenerator : MonoBehaviour
 
     // //warunek mniejszości losowych współrzędnych (x>, z>)
     [SerializeField] Vector3 topRightConstraint;
+
+
+    [SerializeField] uint pointsCount;
 
     //wysokość, na której znajduje się punkt początkowy raycast-a (współrzędna y punktu
     [SerializeField] float raycastBeginPointHeight;
@@ -26,44 +28,58 @@ public class PointsGenerator : MonoBehaviour
     //rzeczywisty punkt znajdujący się na planszy
     private GameObject point;
 
+    private GameObject[] points;
+
     // Start is called before the first frame update
     void Start()
     {
+        points = new GameObject[pointsCount];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!point)
+        for(int i=0; i< pointsCount; i++)
         {
-                //wylosowanie wektora
-                float xRand = Random.Range(bottomLeftConstraint.x, topRightConstraint.x);
-                float zRand = Random.Range(bottomLeftConstraint.z, topRightConstraint.z);
-
-                //punkt początkowy raycast
-                Vector3 rayCastBeginPoint = new Vector3(xRand, raycastBeginPointHeight, zRand);
-
-                RaycastHit[] hits;
-                Ray pointGenerationRay = new Ray(rayCastBeginPoint, Vector3.down);
-
-                //Wykonanie raycast
-                hits = Physics.SphereCastAll(rayCastBeginPoint, pointTemplate.GetComponent<SphereCollider>().radius, Vector3.down,raycastBeginPointHeight * 2.0f);
-
-                var goodHits = new List<RaycastHit>();
-
-                foreach(var hit in hits){
-                    foreach (string surf in allowedSurfaces)    //wykluczenie "złych" trafień
-                    {
-                        if (hit.collider.tag == surf)
-                        {
-                            goodHits.Add(hit);
-                            break;
-                        }
-                    }
-                }
-
-                //wygenerowanie punktu na losowej wysokości
-                point = Instantiate(pointTemplate,goodHits[Random.Range(0,goodHits.Count)].point+offset, Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f)));
-       }   
+            if (points[i] == null)
+            {
+                points[i] = generatePowerUp();
+            }
+        }
     }
+
+    private GameObject generatePowerUp()
+    {
+        //wylosowanie wektora   3.0f - odległość od ścian mapy
+        float xRand = UnityEngine.Random.Range(bottomLeftConstraint.x + 3.0f, topRightConstraint.x - 3.0f);
+        float zRand = UnityEngine.Random.Range(bottomLeftConstraint.z + 3.0f, topRightConstraint.z - 3.0f);
+
+        //punkt początkowy raycast
+        Vector3 rayCastBeginPoint = new Vector3(xRand, raycastBeginPointHeight, zRand);
+
+        RaycastHit[] hits;
+        Ray pointGenerationRay = new Ray(rayCastBeginPoint, Vector3.down);
+
+        //Wykonanie spherecast
+        hits = Physics.SphereCastAll(rayCastBeginPoint, pointTemplate.GetComponent<SphereCollider>().radius, Vector3.down, raycastBeginPointHeight * 2.0f);
+
+        var goodHits = new List<RaycastHit>();
+
+        foreach (var hit in hits)
+        {
+            foreach (string surf in allowedSurfaces)    //wykluczenie "złych" trafień
+            {
+                if (hit.collider.tag == surf)
+                {
+                    goodHits.Add(hit);
+                    break;
+                }
+            }
+        }
+
+        //wygenerowanie punktu na losowej wysokości
+        return Instantiate(pointTemplate, goodHits[UnityEngine.Random.Range(0, goodHits.Count)].point + offset, Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f)));
+    }
+
 }
+
