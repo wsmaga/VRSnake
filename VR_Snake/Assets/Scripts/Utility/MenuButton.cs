7cf6ff397665645ca124cf055ab450f6e3a3cddd
wsmaga <witosma861@student.polsl.pl>
2020-06-19 16:46:06 +0200

    Added nice comments to my scripts.

--- VR_Snake/Assets/Scripts/Utility/MenuButton.cs
+++ VR_Snake/Assets/Scripts/Utility/MenuButton.cs
@@ -7,47 +7,58 @@ using UnityEngine.Events;
 using System.Threading;
 using System;
 
+/// <summary>
+/// Zarządza zachowaniem danego przycisku wrażliwego na Gaze UI - UI sterowane spojrzeniem użytkownika w odpowiednie miejsce.
+/// </summary>
 public class MenuButton : MonoBehaviour
 {
-    public Image circleIndicator;
-    public UnityEvent buttonClickEvent;
-    public float clickTime = 1;
-    bool status = false;
-    public float gazeTimer = 0;
+    public Image circleIndicator;  // Sprite kółeczka sygnalizującego upływający czas w ramach Gaze UI
+    public UnityEvent buttonClickEvent;  // Event wywoływany przy wykryciu dostatecznie długiego wpatrywania się w przycisk
+    public float clickTime = 1;  // Ile sekund powinno zajmować wpatrywanie się w przycisk w celu kliknięcia
+    bool status = false;  // True gdy Gaze UI spoczywa na danym przycisku
+    public float gazeTimer = 0;  // Odlicza czas wpatrywania się w dany przycisk
 
     private void Start()
     {
-        Debug.Log("BUTTON STARTED");
+        // Debug.Log("BUTTON STARTED");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (status)
+        if (status)  // jeżeli user spogląda na dany przycisk...
         {
+            // ...odliczaj czas i aktualizuj wygląd kółeczka:
             gazeTimer += Time.deltaTime;
             circleIndicator.fillAmount = gazeTimer / clickTime;
         }
 
-        if(gazeTimer > clickTime)
+        if(gazeTimer > clickTime)  // jeżeli spoglądamy na przycisk wystarczająco długo...
         {
+            // ...wywołaj event + wyzeruj odpowiednie atrybuty:
             status = false;
             circleIndicator.fillAmount = 0;
             gazeTimer = 0;
-            Debug.Log("BUTTON CLICKED");
+            // Debug.Log("BUTTON CLICKED");
             buttonClickEvent.Invoke();
         }
     }
 
+    /// <summary>
+    /// Wywoływane gdy spojrzenie użytkownika wejdzie w obręb danego przycisku.
+    /// </summary>
     public void GazeOn()
     {
-        Debug.Log("GAZE ON");
+        // Debug.Log("GAZE ON");
         status = true;
     }
 
+    /// <summary>
+    /// Wywoływane gdy spojrzenie użytkownika opuści dany przycisk.
+    /// </summary>
     public void GazeOff()
     {
-        Debug.Log("GAZE OFF");
+        // Debug.Log("GAZE OFF");
         status = false;
         gazeTimer = 0;
         circleIndicator.fillAmount = 0;
--- VR_Snake/Assets/Scripts/Utility/UIController.cs
+++ VR_Snake/Assets/Scripts/Utility/UIController.cs
@@ -5,46 +5,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Zachowanie kontrolera interfejsu użytkownika. W szczególności zarządzanie wyświetlaniem odpowiednich elementów UI i ładowanie wybranych map.
+/// </summary>
 public class UIController : MonoBehaviour
 {
     [SerializeField]
-    Canvas mainMenu;
+    Canvas mainMenu;  // Canvas z UI menu głównego
 
     [SerializeField]
-    Canvas mapSelect;
+    Canvas mapSelect;  // Canvas z UI menu wyboru map
 
     [SerializeField]
-    GameObject world;
+    GameObject world;  // Obiekt zawierający wszystkie widzialne obiekty wokół gracza dla łatwego obracania światem wokół gracza
 
     [SerializeField]
-    GameObject camera;
+    GameObject camera;  // Kamera będąca de facto oczami gracza korzystającego z gogli VR
 
-    private const float WORLDROTSPEED = 0.01f;
+    private const float WORLDROTSPEED = 0.01f;  // Jak szybko świat obraca się gdy gracz zgubi menu z zasięgu wzroku
 
     public void Start()
     {
-        ShowMainMenu();
+        ShowMainMenu();  // Na początku programu wyświetl graczowi menu główne
     }
 
     public void Update()
     {
+        // Ustal wektory kierunkowe gracza oraz świata wokół niego:
         Vector3 worldDir = world.transform.forward;
         Vector3 playerDir = camera.transform.forward;
 
-        if (Vector3.Angle(worldDir, playerDir) > 70)
+        if (Vector3.Angle(worldDir, playerDir) > 70)  // Jeżeli kąt między tymi wektorami jest zbyt duży...
         {
+            // ...obróć świat w stronę gracza tak by ten nie stracił UI z oczu:
             Vector3 direction = Vector3.RotateTowards(worldDir, playerDir, WORLDROTSPEED, 1000);
             direction.y = 0;
             world.transform.rotation = Quaternion.LookRotation(direction);
         }
     }
 
+    /// <summary>
+    /// Wywoływana gdy gracz chce wyjść z aplikacji.
+    /// </summary>
     public void ExitGame()
     {
-        Debug.Log("EXITING APPLICATION");
+        // Debug.Log("EXITING APPLICATION");
         Application.Quit(0);
     }
 
+    /// <summary>
+    /// Wywoływana gdy gracz chce wyświetlić menu wyboru map. Wyświetla odpowiednie canvasy.
+    /// </summary>
     public void ShowMapSelect()
     {
         mainMenu.gameObject.SetActive(false);
@@ -52,6 +63,9 @@ public class UIController : MonoBehaviour
         mapSelect.gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// Wywoływana gdy gracz chce wyświetlić menu główne. Wyświetla odpowiednie canvasy.
+    /// </summary>
     public void ShowMainMenu()
     {
         mapSelect.gameObject.SetActive(false);
@@ -59,6 +73,10 @@ public class UIController : MonoBehaviour
         mainMenu.gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// Wywoływana gdy gracz wybierze konkretną mapę do załadowania.
+    /// </summary>
+    /// <param name="map">Nazwa sceny z wybraną mapą.</param>
     public void StartMap(string map)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(map, UnityEngine.SceneManagement.LoadSceneMode.Single);
